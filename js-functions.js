a7b220d88e8057068982631971e7718972f7cb69
dineshchand1984 <42776668+dineshchand1984@users.noreply.github.com>
2021-09-13 20:58:52 +0530

    Add files via upload

--- js-functions.js
+++ js-functions.js
@@ -1,14 +1,14 @@
 function AddNotes(){
-	console.log(" function - addNewNote() : start");
+  console.log(" function - addNewNote() : start");
 
-	var taskstable = document.getElementById("taskstable");
-	var taskrow = taskstable.insertRow();
-	var cellId  = taskrow.insertCell(0);
-	var cellTaskDesc = taskrow.insertCell(1);
-	cellId.innerHTML = "<input type =\"checkbox\" class=\"taskid-row\"  />";
-	cellTaskDesc.innerHTML = "<input class=\"taskdesc-row\" />";
-	
-	console.log(" function - addNewNote() : end");
+  var taskstable = document.getElementById("taskstable");
+  var taskrow = taskstable.insertRow();
+  var cellId  = taskrow.insertCell(0);
+  var cellTaskDesc = taskrow.insertCell(1);
+  cellId.innerHTML = "<input type =\"checkbox\" class=\"taskid-row\"  />";
+  cellTaskDesc.innerHTML = "<input class=\"taskdesc-row\" />";
+
+  console.log(" function - addNewNote() : end");
 }
 
 function DeleteNotes(){
@@ -17,21 +17,21 @@ function DeleteNotes(){
   var tasksTable = document.getElementById("taskstable");
   var numberOfNotes = tasksTable.rows.length - 1;
   for (var i = numberOfNotes; i > 0; i--) {
-	 var row = tasksTable.rows[i];
-	 var chkbox = row.getElementsByTagName('input')[0];
-	 if( 'checkbox' == chkbox.type && true == chkbox.checked)
-		tasksTable.deleteRow(i);
+     var row = tasksTable.rows[i];
+     var chkbox = row.getElementsByTagName('input')[0];
+     if( 'checkbox' == chkbox.type && true == chkbox.checked)
+        tasksTable.deleteRow(i);
   }
-  
+
   console.log(" function - deleteNote() : end");
 }
 
 function SaveNotes(){
-	console.log(" function - saveNotes() : start");
+  console.log(" function - saveNotes() : start");
 
-	var taskstable    = document.getElementById("taskstable");
-	var numberOfNotes = taskstable.rows.length - 1;
-	console.log(" Number of notes : " + numberOfNotes);
+  var taskstable    = document.getElementById("taskstable");
+  var numberOfNotes = taskstable.rows.length - 1;
+  console.log(" Number of notes : " + numberOfNotes);
 
-	console.log(" function - saveNotes() : end");
+  console.log(" function - saveNotes() : end");
 }
